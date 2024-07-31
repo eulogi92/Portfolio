61896e75fb594f82ef3298efc3b6fdd00408133c
Marco Eulogi <marco.eulogi@gmail.com>
2024-07-31 19:18:58 +0200

    Add files via upload

---
+++ SQL/European_Soccer_Database/European_Soccer_Database.sql
@@ -0,0 +1,84 @@
+-- Calculate how many days have passed from the oldest Match to the most recent one
+
+select 
+date_diff(max(date), min(date), day) as days_passed
+from compito1.match;
+
+-- 4 Produce a table which, for each Season and League Name, shows the following statistics about the home goals scored:  min average  mid-range  max  sum
+
+select 
+m.season, 
+l.name,
+min(m.home_team_goal) as min,
+round(avg(m.home_team_goal),2) as avg, 
+max(m.home_team_goal) as max,
+sum(m.home_team_goal) as sum,
+from db.match as m
+  join db.leagues as l
+  on m.league_id=l.id
+group by m.season, l.name
+order by sum DESC;
+
+-- Show, for each Season, the number of matches played by each League
+
+select
+m.season, 
+l.name,
+count (m.id) as n_matches
+from db.match as m
+  join db.leagues as l
+  on m.league_id=l.id
+group by m.season, l.name;
+
+-- Calculate the body mass index (BMI) of the players
+
+select *,
+round(weight/2.205,2) as kg_weight,
+round(height/100,2) as m_height,
+weight/2.205/((height/100)*(height/100)) as BMI
+from db.playerBMI
+where weight/2.205/((height/100)*(height/100)) between 18.5 and 24.9
+
+-- 7 How many players do not have an optimal BMI (<18.5 or or >24.9)?
+
+select *,
+round(weight/2.205,2) as kg_weight,
+round(height/100,2) as m_height,
+weight/2.205/((height/100)*(height/100)) as BMI
+from db.playerBMI
+where weight/2.205/((height/100)*(height/100))<18.5 or weight/2.205/((height/100)*(height/100))>24.9
+
+
+-- Identify which Team has scored the highest total number of goals (home + away) during the most recent available season
+select
+m.season, 
+t.team_long_name,
+sum(m.home_team_goal)+sum(m.away_team_goal) as tot_goals
+from db.match as m
+  join db.team as t
+  on m.home_team_api_id=t.team_api_id or m.away_team_api_id=t.team_api_id
+group by m.season, t.team_long_name
+order by m.season, tot_goals DESC;
+
+
+--9 Create a query that, for each season, shows the name of the team that ranks first in terms of total goals scored. 
+
+with tabella as (
+  select *,
+  rank() over (partition by season order by tot_goals DESC) as ranking_per_season
+  from (  
+    select
+    m.season, 
+    t.team_long_name,
+    sum(m.home_team_goal)+sum(m.away_team_goal) as tot_goals
+    from db.match as m
+      join db.team as t
+      on m.home_team_api_id=t.team_api_id or m.away_team_api_id=t.team_api_id
+    group by m.season, t.team_long_name
+    order by m.season, tot_goals DESC)
+    order by season
+)
+
+select *
+from tabella 
+where ranking_per_season = 1
